a651d528288795c9db04fb4585366f0df01909b7
ittk-93 <gaiag5245@gmail.com>
2022-06-03 16:59:17 +0900

    pythonのinはjavascriptのincludes

--- static/js/validation.js
+++ static/js/validation.js
@@ -2,7 +2,7 @@ function validation() {
   const url = document.forms[0].keywords.value;
   const domain = url.split("/")[2];
   const domains = ["www.youtube.com", "m.youtube.com", "youtu.be"];
-  if (url == "" || !domain in domains) {
+  if (url == "" || !domains.includes(domain)) {
     alert("YouTube動画のURLを入力してください");
     return false;
   } else {
